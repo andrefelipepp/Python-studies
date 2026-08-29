0f416a28972039663e7f15a851d6dd1599d4433b
André Felipe <144925799+andrefelipepp@users.noreply.github.com>
2026-08-29 02:53:09 +0000

    Tipos de Dados Basicos

---
+++ tipos_dados_basicos.py
@@ -0,0 +1,26 @@
+# Os números inteiros são aqueles sem casas decimais, como 1, 42, -10. 
+# Em Python, eles são representados pelo tipo int.
+
+numero_inteiro = 10
+print(type(numero_inteiro)) # <class 'int'>
+
+# Os floats são números com casas decimais. Por exemplo, 3.14, 0.5 e
+# -7.0. Eles são úteis para cálculos mais precisos, como medições e 
+# valores monetários.
+
+numero_float = 3.14
+print(type(numero_float)) # <class 'float'>
+
+# Uma string é uma sequência de caracteres, usada para representar texto. 
+# Pode ser qualquer coisa que você coloca entre aspas simples ' ' ou 
+# duplas " ".
+
+texto = "olá, python!"
+print(type(texto)) # <class 'str'>
+
+# O tipo bool é usado para representar valores verdadeiros ou falsos, 
+# com apenas dois possíveis valores: True ou False. Eles são usados em 
+# estruturas de controle de fluxo, como condicionais.
+
+valor_bool = True
+print(type(valor_bool)) # <class 'bool'>
