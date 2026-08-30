2ccc21f562f8129b3cc3aea800d43cafd6f3f8b4
André Felipe <144925799+andrefelipepp@users.noreply.github.com>
2026-08-30 14:17:39 +0000

    Entendendo variaveis

---
+++ variaveis.py
@@ -0,0 +1,16 @@
+# As variáveis são como "caixas" onde você armazena valores 🗃️. Elas permitem que você manipule dados de forma dinâmica durante 
+# a execução do seu programa.
+
+idade = 23 # A variável 'idade' agora contém um inteiro
+nome = "Felipe" # A variável 'nome' agora contém uma string
+peso = 70.00 # # A variável 'peso' agora contém uma float
+
+### Regras para nomear variáveis
+
+# Devem começar com uma letra ou `_` (underline).
+# Não podem começar com um número.
+# Não podem conter espaços ou caracteres especiais como `@`, `#`, `!`.
+# São **case-sensitive** (`idade` e `Idade` são variáveis diferentes).
+
+# 💡 Imagine as variáveis como "etiquetas" que você coloca em caixas 📦. A etiqueta "idade" pode estar associada ao número `23`,
+# e a etiqueta "nome" ao texto `"Felipe"`.
