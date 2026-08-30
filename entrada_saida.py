774df4bcaa29a2496e4a9d9c120f1987b9d068d6
André Felipe <144925799+andrefelipepp@users.noreply.github.com>
2026-08-30 14:25:57 +0000

    Implementado entradas e saidas com print() e input()

---
+++ entrada_saida.py
@@ -0,0 +1,18 @@
+# A comunicação entre você e seu programa é feita com as funções input() e print().
+
+# Saída de dados com print()
+print("Hello World!") #A função print() é usada para exibir informações na tela.
+
+### Entrada de dados com `input()`
+
+# A função `input()` permite que o usuário insira dados. O valor inserido é sempre do tipo `string`.
+
+nome = input("Qual o seu nome? ")
+print("Me nome é " + nome + " e o seu?")
+
+# 💡 Dica: Quando precisar de um número do usuário, converta o valor de input() usando int() ou float().
+idade = int(input("Qual a sua idade Miguel ? "))
+print(idade)
+
+
+
