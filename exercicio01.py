34a43d94be676c75b03ac7dd8ee8c9287d1e2f79
André Felipe <144925799+andrefelipepp@users.noreply.github.com>
2026-09-10 17:01:46 +0000

    Exercicio 01

---
+++ exercicio01.py
@@ -0,0 +1,12 @@
+# Crie um programa que solicite ao usuário que insira seu nome e idade. 
+# Em seguida, exiba uma mensagem dizendo: "Olá, [nome], você tem [idade] anos!".
+
+nome = (input("Qual o seu nome ? "))
+idade = int(input("Qual a sua idade ? "))
+
+print(f"Olá, {nome}, você tem {idade} anos! ")
+
+cidade = input("Onde você nasceu? ")
+
+print(f"Eu nasci em {cidade}")
+
