6ea65e03cc3909156c720d00317c366627b8827b
André Felipe <144925799+andrefelipepp@users.noreply.github.com>
2026-09-10 17:39:42 +0000

    Exercicio 02

---
+++ exercicio02.py
@@ -0,0 +1,15 @@
+# Crie um programa que leia dois números do usuário e exiba a soma, subtração, 
+# multiplicação e divisão desses números.
+
+numero1 = float(input("Digite um numero: "))
+numero2 = float(input("Digite um segundo numero: "))
+
+soma = numero1 + numero2
+subtracao = numero1 -numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+
+print(f"O resultado da soma é {soma}")
+print(f"O resultado da subtração é {subtracao}")
+print(f"O resultado da Multiplicação é {multiplicacao}")
+print(f"O resultado da Divisão é {divisao}")
