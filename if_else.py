71df411102739ebf3131f2b9a038544496272af0
André Felipe <144925799+andrefelipepp@users.noreply.github.com>
2026-09-11 23:34:37 +0000

    Condicionais IF e ELSE

---
+++ if_else.py
@@ -0,0 +1,13 @@
+idade = int(input("Digite sua idade: "))
+
+if(idade >= 18):
+    print("Maior de idade")
+else:
+    print("Menor de idade")
+
+estiverChovendo = True
+
+if(estiverChovendo):
+    print("Levar guarda-chuva")
+else:
+    print("Não levar guarda-chuva")
